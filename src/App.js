81911efb19760e65653c4460df92a04adfcd79eb
Roland Enola <40168844+mystiexx@users.noreply.github.com>
2020-10-15 19:00:32 +0100

    new commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header/Header";
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+import Dashboard from "./components/Dashboard/Dashboard";
+
+function App() {
+    return (
+        <Router>
+            <div className="App">
+                <Switch>
+                    <Header>
+                        <Route exact path="/" component={Dashboard} />
+                    </Header>
+                </Switch>
+            </div>
+        </Router>
+    );
+}
+
+export default App;
---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import {Container} from 'react-bootstrap'
+
+class Dashboard extends Component {
+    render() {
+        return (
+            <div>
+                <Container style={{ marginTop: 110, marginLeft: 10 }}>
+                <h1>Hello World</h1>
+                </Container>
+            </div>
+        );
+    }
+}
+
+export default Dashboard;
